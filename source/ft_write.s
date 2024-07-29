950c3584c011fcac3a8cc315e4d4dcda7e9cdbb7
Sasha <sasha.lorion@gmail.com>
2024-07-29 15:40:13 +0200

    [NEW] - Create and finish

--- source/ft_write.s
+++ source/ft_write.s
@@ -1,4 +1,4 @@
-__errno_location
+extern __errno_location
 global ft_write             ; ssize_t write(int fd, const void buf, size_t count)
 
 ft_write:
@@ -11,8 +11,8 @@ ft_write:
 
 err:
     neg rax                     ; Convert the regative value to a positive error code
-    mov rdi, rax                ; Move the error code to rdi register (for errno_location call)
+    mov rdi, [rax]                ; Move the error code to rdi register (for errno_location call)
     call __errno_location         ; Call errno_location
-    mov [rax], rdi              ; Get the reference of rdi to store the error code in rax register
+    mov rax, [rdi]              ; Get the reference of rdi to store the error code in rax register
     mov rax, -1                 ; Retunr -1 for indicate an error
     ret                         ; Return the error code
